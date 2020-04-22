62b8af88e1daf13dcce55badcdf01956aa36657c
FourthTee <fourthtee@DESKTOP-NT3FRF5.localdomain>
2020-04-21 18:50:34 -0700

    finish process.py

--- process.py
+++ process.py
@@ -4,11 +4,16 @@ from random import *
 line = sys.stdin.readline() 
 linecount = 1
 while line:
+    #Check if its the 10th line
     if (linecount % 10 == 0) :
         print(line)
+    
+    #Generate random number between 0 to 1
     check = random()
     if (check >= 0.5):
         print(line, file=sys.stderr)
+
+    #move to next line and increment line counter
     line = sys.stdin.readline()
     linecount += 1
 
